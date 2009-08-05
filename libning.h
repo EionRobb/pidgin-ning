8670f35d3fc3879d91021125a5505ac4333effa3
eionrobb <eionrobb@3d2ad310-7e29-11de-ae3e-cdf4340e3b18>
2009-08-05 10:07:33 +0000

    Initial commit.  Log into Ning?

---
+++ libning.h
@@ -0,0 +1,95 @@
+/*
+ * libning
+ *
+ * libning is the property of its developers.  See the COPYRIGHT file
+ * for more details.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBNING_H
+#define LIBNING_H
+
+#define NING_PLUGIN_VERSION "0.01"
+
+#include <glib.h>
+
+#include <errno.h>
+#include <string.h>
+#include <glib/gi18n.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <json-glib/json-glib.h>
+
+#ifndef G_GNUC_NULL_TERMINATED
+#	if __GNUC__ >= 4
+#		define G_GNUC_NULL_TERMINATED __attribute__((__sentinel__))
+#	else
+#		define G_GNUC_NULL_TERMINATED
+#	endif /* __GNUC__ >= 4 */
+#endif /* G_GNUC_NULL_TERMINATED */
+
+#ifdef _WIN32
+#	include "win32dep.h"
+#	define dlopen(a,b) LoadLibrary(a)
+#	define RTLD_LAZY
+#	define dlsym(a,b) GetProcAddress(a,b)
+#	define dlclose(a) FreeLibrary(a)
+#else
+#	include <arpa/inet.h>
+#	include <dlfcn.h>
+#	include <netinet/in.h>
+#	include <sys/socket.h>
+#endif
+
+#ifndef PURPLE_PLUGINS
+#	define PURPLE_PLUGINS
+#endif
+
+#include "accountopt.h"
+#include "connection.h"
+#include "debug.h"
+#include "dnsquery.h"
+#include "proxy.h"
+#include "prpl.h"
+#include "request.h"
+#include "sslconn.h"
+#include "version.h"
+
+
+typedef struct _NingAccount NingAccount;
+
+struct _NingAccount {
+	PurpleAccount *account;
+	PurpleConnection *pc;
+	GHashTable *hostname_ip_cache;
+	GSList *conns; /**< A list of all active FacebookConnections */
+	GSList *dns_queries;
+	GHashTable *cookie_table;
+	
+	time_t last_messages_download_time;
+	
+	gchar *xg_token; /**< Used for finding information from host site */
+	
+	gchar *ning_app;
+	gchar *ning_id;
+	
+	gchar *chat_domain;
+	gchar *chat_token;
+};
+
+
+
+#endif /* LIBNING_H */
